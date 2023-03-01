7cd7dc9ec3053657045e7ea52d3fe14a7c7462ff
USER-cat <luka.khorava@gmail.com>
2023-03-01 20:02:27 +0400

    JS.MAPS

---
+++ script.js
@@ -0,0 +1,76 @@
+// function baraworks(num){
+//     return num*2;
+// }
+
+// console.log(baraworks(2));
+
+
+// const fun = (num) =>{ return num*2 ;}
+
+// console.log(fun(12));   
+
+
+// const array = [1,2,3,4,5,6,7,];
+
+// const double_numbers = numbers.map((ricxvi) => {
+//     return ricxvi *2 ;
+// });
+
+// const array = [20,40,60];
+
+// const new_array = array.map(function(num){
+//     return num*2;
+// })
+// const new_array2 = array.map((num) =>  num*2);
+// console.log(new_array);
+// console.log(new_array2);
+
+
+// const names = ['Luka' , 'Friadtsa' , 'Luna' , 'Max' , '2bital'];
+
+// const namesWithXY = names.map((array , index) => {
+//     if(index % 2 === 0){
+//         return (array += " X");
+//     } else {
+//         return (array += " Y");
+//     }
+// })
+
+// console.log(namesWithXY);
+
+
+/* 
+ --------------------- FILTER -------------------
+// */
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// // const new_numbers = numbers.filter((num) => num % 2 ===0
+// // )
+// const new_numbers = numbers.filter((num) => {
+//     return num > 5;
+// })
+// console.log(new_numbers);
+
+
+// const words = ['Kata' , 'Katanebs' , '2B' , 'Varskvlavt' , 'Mankanebs' , 'Ca' , "ukidegano"];
+
+// const new_words = words.filter((str) => str.length > 2);
+
+// console.log(new_words);
+
+// /*
+// --------------Find---------------- 
+// */
+
+
+// const animals = ["Bu" , "Kata" , "Vepkhvi" , "Lomi"];
+
+// const found_animal = animals.find((str) => str === 'Lomi');
+
+// console.log(found_animal);
+// const animals = ["Bu" , "Kata" , "Vepkhvi" , "Lomi"];
+
+// const found_animal = animals.findIndex((str) => str === 'Lomi');
+
+// console.log(found_animal);

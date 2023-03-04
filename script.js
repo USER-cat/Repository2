09ec210274f2751d55c8024f606834e005ed9a3b
Jungle Coding Cat <43236755+USER-cat@users.noreply.github.com>
2023-03-05 00:01:50 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,154 +1,108 @@
-/* 
-------JAVASCRIPT OBJECTS------------
+// let p = document.querySelector("h1");
 
-*/
+// p.innerHTML = "From JS";
+// // p.style.backgroundColor = 'red' ;
+// p.classList= ("green_box")
 
+// const paragraphs_list = document.querySelectorAll("h1");
 
-// let person1 = {
-//     firstName: 'JON',
-// };
+// for (let i= 0 ; i < paragraphs_list.length; i++){
+//     paragraphs_list[i].innerHTML ='varskvlavebi ubeebshi chaibargo';
+// }
 
-// function some(arg,lastName){
-//     arg.lastName = lastName;
+// const switchBitch = document.querySelectorAll(".targo");
+
+// for(let i = 0; i < switchBitch.length ; i++){
+//     switchBitch[i].innerHTML = '<i>Every night in my dreams</i>';
 // }
 
 
-// some(person1, '2Bital');
-// console.log(person1);
 
+// let counter = document.getElementById("number");
 
+// let count = 0;
+// counter.innerHTML = count;
 
-// -------------TASKS--------------------
-// 1)
-// function OddOrEven(number){
-//     if (number%2 === 0 ){
-//         console.log("ლუწია");
-//     }else{
-//         console.log("კენტია");
-//     }
+// function goUpBaby(){
+// count += 1;
+// counter.innerHTML = count;
 // }
 
-// OddOrEven(8);
-
-
-// 2)
-
-// const array = [1,2,3,4,5,6,7,-2,-5,3];
-// let sum = 0;
-// for (let i = 0 ; i < array.length; i++){
-//     if(array[i]>0){
-//          sum += array[i];
-//     }
-//     console.log(sum);
+// function goDownBaby(){
+//     if(count>0){
+//     count -= 1; 
+//     counter.innerHTML = count;}
 // }
 
 
-// 3)
-
-// function positiveToNegative(number){
-//    return number -= 2*number;
+// function increaseBy(num){
+//     count *= num;
+//     counter.innerHTML = count;
 // }
 
-// console.log(positiveToNegative(8.3));
 
-
-// 4) 
-
-
-// function nameReverse(name){
-//   let nameSplit =  name.split("");
-//   let nameReverse =  nameSplit.reverse();
-//   let nameJoin =  nameReverse.join("");
-//     return nameJoin;
+// function reset(){
+//     count = 0;
+//     counter.innerHTML = count;
 // }
 
-// console.log(nameReverse("Friadtsa"));
-
-// function reverseString(str){
-//     let newString = "";
-//     for (let i = str.length -1 ; i >= 0 ; i--){
-//         newString += str[i];
-//     }
-//     return newString;
-// }
-
-// console.log(reverseString('Friadtsa'));
-
-// 5)
-// function trueOrFalse(arg){
-//     if(arg === true){
-//         console.log("Yes");
-//     }else if(arg === false){
-//        console.log("No");
-//     }
-// }
-// 
-// trueOrFalse(5%2===1);
-
-// 6)
-
-// function viceVersa(num){
-//     return -num;
-// }
-
-// console.log(viceVersa(-2));              
-
-// 7)
-
-// function numberToString(num){
-//     return num.toString();
-// }
-
-// console.log(numberToString(12));
-// console.log(12);
-
-// 8)
-
-// function killFirstAndLast(str){
-//     let array = str.split("");
-//     let killFirst = array.shift();
-//     let killLast = array.pop();
-//     let deadWord = array.join("");
-//     return deadWord;
-// }
-
-// console.log(killFirstAndLast("Riodejaneiro"));
-
-
-// let name = 'Rio'
-// let array = name.split("");
-// let deadName = array.pop();
-// console.log(array);
-
-
-// 9)
-
-// function oddOrEven(str){
-//     let strArray = str.split("");
-//     if (strArray.length % 2 !== 0){
-//     let strArrayOdd = strArray[Math.round((strArray.length-1)/2)];
-//     return strArrayOdd;
-// }
-//     else if(strArray.length % 2 == 0){
-//         let strArrayEven1 = strArray[Math.floor((strArray.length - 1)/2)];
-//         let strArrayEven2 = strArray[Math.floor((strArray.length + 1)/2)];
-//         let strArrayEven = strArrayEven1+strArrayEven2;
-//         return strArrayEven;
-//     }
-    
-// }
-
-// console.log(oddOrEven("ჰიპოპოტომონსტროსესკიფითალოფობია"));
 
 
 
-// let word = "kenti";
-// let wordArray = word.split("");
-// let logMiddleLetter = wordArray[Math.round((wordArray.length-1)/2)];
-// console.log(logMiddleLetter);
 
-// let array = ["item" , "item2"  , "item3" , "item4" , "item5"];
 
-// let logMiddle = array[Math.round((array.length-1)/2)];
-// console.log(logMiddle);
+// NEED TO FINISH VERY INTERESTING WORK 
+//  STOPPED AT LECTURE 31 / 57:00 
+
+
+// const decreaseBTN = document.getElementById("decrease")
+// const increaseBTN = document.getElementById("increase")
+// const increaseTwiceBTN = document.getElementById("increaseTwice")
+// const resetBTN = document.getElementById("reset");
+// const counterParagraph = document.getElementById("number");
+
+// let count = 0;
+// counterParagraph.innerHTML = count ;
+
+// increaseBTN.addEventListener("click", function(){
+//     count += 1;
+//     counterParagraph.innerHTML = count ;
+// })
+
+// decreaseBTN.addEventListener("click", function(){
+//     if(count>0){
+//     count -= 1;
+//     counterParagraph.innerHTML = count ;
+// }})
+
+// increaseTwiceBTN.addEventListener("click", function(){
+//     count += 2;
+//     counterParagraph.innerHTML = count ;
+// })
+
+// resetBTN.addEventListener("click", function(){
+//     count = 0;
+//     counterParagraph.innerHTML = count ;
+// })
+
+const boxBox = document.getElementById("box")
+const greenBTN = document.getElementById("green");
+const whiteBTN = document.getElementById("white");
+const purpleBTN = document.getElementById("purple");
+
+function buttonToGreen(){
+    boxBox.style.background = "green";
+}
+
+greenBTN.addEventListener("click", () => buttonToGreen());
+
+whiteBTN.addEventListener("click" ,function(){
+    boxBox.style.backgroundColor = "white";
+})
+
+purpleBTN.addEventListener("click" ,function(){
+    boxBox.style.backgroundColor = "purple";
+})
+
+
 
